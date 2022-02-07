b7c2cd3543522b855aa9c30b93e9d44d2c41f435
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-02-07 15:44:31 +0100

    creation et liaison pour les nouvelles pages

---
+++ public/js/boissons.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('retour').addEventListener('click', function(){
+        document.location.href = './';
+    });
+
+});
