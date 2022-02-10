3ad3d308e728f250f584154fe9369e0e16f0207c
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-02-10 21:46:14 +0100

    ajout du PDO

---
+++ public/classes/MyPDO.php
@@ -0,0 +1,21 @@
+<?php
+
+class MyPDO {
+    private PDO $pdo;
+    private PDOStatement $pdos_select;
+    private PDOStatement $pdos_update;
+    private PDOStatement $pdos_insert;
+    private PDOStatement $pdos_delete;
+    private PDOStatement $pdos_selectAll;
+    private string $nomTable;
+
+    public function __construct($sgbd, $host, $db, $user, $password, $nomTable)
+    {
+        $this->pdo = new PDO("mysql:host=".$host.";dbname=".$db, $user, $password);
+
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    }
+}
+
+?>
