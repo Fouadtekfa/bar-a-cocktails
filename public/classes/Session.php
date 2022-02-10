970074cd91e46e4e158d53b61ca67265146f7485
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-02-10 23:43:57 +0100

    Enlevement des commentaires

--- public/classes/Session.php
+++ public/classes/Session.php
@@ -6,11 +6,3 @@ if(!isset($_GET['action']))
     $_GET['action'] = 'init';
 
 $myPDO = new MyPDO($_ENV['sgbd'], $_ENV['host'], $_ENV['db'], $_ENV['user'], $_ENV['pwd']);
-/*$myPDO->setNomTable('Etape');
-echo 'hola';
-$myPDO->initPDOS_selectAll();
-$va =  $myPDO->getAll();
-
-foreach ($va as $valeur){
-    echo $valeur->getCNom();
-}*/
