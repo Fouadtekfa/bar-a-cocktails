51eee10e296f56b580b35248c25a9b0e550cb253
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-02-11 22:41:50 +0100

    ajout du template de formulaire

--- public/Vue/utensiles.php
+++ public/Vue/utensiles.php
@@ -23,6 +23,9 @@ $va =  $myPDO->getAll();
     <img src="../images/retour.png" alt="retour" class="retour">
     </i>
     </div>
+
+    <div id="informationEntite">
+    <button type="button" class="btn btn-primary" id="btn_ajouter">Ajouter</button>
     <div class="tableContainer">
         <table class="table">
             <thead class="thead-dark">
@@ -38,7 +41,6 @@ $va =  $myPDO->getAll();
                         <tr>
                             <th scope="row"><?php echo $valeur->getUId() ?></th>
                             <td><?php echo $valeur->getUNom() ?></td>
-                            <td class="td_buttons_actions"><button type="button" class="btn btn-primary">Ajouter</button></td>
                             <td class="td_buttons_actions"><button type="button" class="btn btn-warning">Editer</button></td>
                             <td class="td_buttons_actions"><button type="button" class="btn btn-danger">Supprimer</button></td>
                         </tr>
@@ -47,7 +49,27 @@ $va =  $myPDO->getAll();
             </tbody>
         </table>    
     </div>
+    </div>
 
+    <div class="insertContainer" style="display: none" id="insertContainer">
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
