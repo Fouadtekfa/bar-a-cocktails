7778e6f7ef4b149665b32bc773a672e5839e3951
Nadia Hariri <nadia.hariri@etu.univ-lehavre.fr>
2022-02-14 14:04:26 +0100

    Dérnière modification de MyPDO

--- public/Controlleur/MyPDO.php
+++ public/Controlleur/MyPDO.php
@@ -66,6 +66,171 @@ class MyPDO {
     $this->pdos_update =  $this->pdo->prepare($query);
 }
 
+
+public function update(string $id, array $assoc): void {
+    if (! isset($this->pdos_update))
+        $this->initPDOS_update($id, array_keys($assoc));
+    foreach ($assoc as $key => $value) {
+        $this->getPdosUpdate()->bindValue(":".$key, $value);
+    }
+    $this->getPdosUpdate()->execute();
+}
+
+public function initPDOS_insert(array $colNames): void {
+    $query = "INSERT INTO ".$this->nomTable." VALUES(";
+    foreach ($colNames as $colName) {
+        $query .= ":".$colName.",";
+    }
+    $query = substr($query,0, strlen($query)-2);
+    $query .= ')';
+    echo $query;
+    $this->pdos_insert = $this->pdo->prepare($query);
+}
+public function insert(array $assoc): void {
+    if (! isset($this->pdos_insert))
+        $this->initPDOS_insert(array_keys($assoc));
+    foreach ($assoc as $key => $value) {
+        $this->getPdosInsert()->bindValue(":".$key, $value);
+    }
+    $this->getPdosInsert()->execute();
+}
+
+public function initPDOS_delete(string $nomColId = "Code"): void {
+    $this->pdos_delete = $this->pdo->prepare("DELETE FROM ". $this->nomTable
+                                                  ." WHERE $nomColId=:".$nomColId);
+}
+
+
+public function delete(array $assoc) {
+    if (! isset($this->pdos_delete))
+        $this->initPDOS_delete(array_keys($assoc)[0]);
+    foreach ($assoc as $key => $value) {
+        $this->getPdosDelete()->bindValue(":".$key, $value);
+    }
+    $this->getPdosDelete()->execute();
+}
+public function initPDOS_count() {
+    $this->pdos_count = $this->pdo->prepare("SELECT COUNT(*) FROM ".$this->nomTable);
+
+}
+
+public function count() {
+  if (! isset($this->pdos_count))
+    $this->initPDOS_count();
+
+     $this->getPdosCount()->execute();
+
+}
+public function getCountValue() : int {
+    $this->count();
+    return $this->pdos_count->fetch(PDO::FETCH_NUM)[0];
+  }
+  public function getPdo(): PDO
+    {
+        return $this->pdo;
+    }
+
+    public function getPdosSelect() : PDOStatement
+    {
+        return $this->pdos_select;
+    }
+
+
+
+    public function getPdosSelectAll(): PDOStatement
+    {
+        return $this->pdos_selectAll;
+    }
+
+    /**
+     * @param PDOStatement $pdos_select
+     * @return MyPDO
+     */
+    public function setPdosSelect(PDOStatement $pdos_select): MyPDO
+    {
+        $this->pdos_select = $pdos_select;
+        return $this;
+    }
+
+    /**
+     * @return PDOStatement
+     */
+    public function getPdosUpdate(): PDOStatement
+    {
+        return $this->pdos_update;
+    }
+
+    /**
+     * @param PDOStatement $pdos_update
+     * @return MyPDO
+     */
+    public function setPdosUpdate(PDOStatement $pdos_update): MyPDO
+    {
+        $this->pdos_update = $pdos_update;
+        return $this;
+    }
+
+    /**
+     * @return PDOStatement
+     */
+    public function getPdosInsert(): PDOStatement
+    {
+        return $this->pdos_insert;
+    }
+
+    /**
+     * @param PDOStatement $pdos_insert
+     * @return MyPDO
+     */
+    public function setPdosInsert(PDOStatement $pdos_insert): MyPDO
+    {
+        $this->pdos_insert = $pdos_insert;
+        return $this;
+    }
+
+    /**
+     * @return PDOStatement
+     */
+    public function getPdosDelete(): PDOStatement
+    {
+        return $this->pdos_delete;
+    }
+
+    /**
+     * @param PDOStatement $pdos_delete
+     * @return MyPDO
+     */
+    public function setPdosDelete(PDOStatement $pdos_delete): MyPDO
+    {
+        $this->pdos_delete = $pdos_delete;
+        return $this;
+    }
+
+    
+
+    public function getPdosCount(): PDOStatement
+    {
+        return $this->pdos_count;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNomTable(): string
+    {
+        return $this->nomTable;
+    }
+
+    /**
+     * @param string $nomTable
+     * @return MyPDO
+     */
+    public function setNomTable(string $nomTable): MyPDO
+    {
+        $this->nomTable = $nomTable;
+        return $this;
+    }
+
 }
 
 ?>
