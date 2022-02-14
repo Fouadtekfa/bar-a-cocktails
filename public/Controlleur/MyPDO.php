9b055055273c1138debc9f28cacffd703298b334
Nadia Hariri <nadia.hariri@etu.univ-lehavre.fr>
2022-02-14 14:46:54 +0100

    Faire des revert sur certain commit

---
+++ public/Controlleur/MyPDO.php
@@ -0,0 +1,171 @@
+<?php
+
+class MyPDO {
+    private PDO $pdo;
+    private PDOStatement $pdos_select;
+    private PDOStatement $pdos_update;
+    private PDOStatement $pdos_insert;
+    private PDOStatement $pdos_delete;
+    private PDOStatement $pdos_selectAll;
+    private string $nomTable;
+
+    public function __construct($sgbd, $host, $db, $user, $password){
+        $this->pdo = new PDO("mysql:host=".$host.";dbname=".$db, $user, $password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function initPDOS_selectAll() {
+        $this->pdos_selectAll = $this->pdo->prepare('SELECT * FROM '.$this->nomTable);
+    }
+
+
+
+    public function getAll(): array {
+        if (!isset($this->pdos_selectAll))
+            $this->initPDOS_selectAll();
+        $this->getPdosSelectAll()->execute();
+        return $this->getPdosSelectAll()->fetchAll(PDO::FETCH_CLASS,
+            "bar\Entite".ucfirst($this->getNomTable()));
+    }
+    public function initPDOS_select(int $id): void
+    {
+        $requete = "SELECT * FROM ".$this->nomTable ." WHERE ";
+        /*foreach($id as $key=>$val){
+            $requete .= "" . $key ."= :" .$key." AND ";
+        * a corriger apres
+         * /
+        $requete = "SELECT * FROM ".$this->nomTable ." WHERE " ;
+        $requete = substr($requete,0, strlen($requete)-4);
+        $this->pdos_select = $this->pdo->prepare($requete);
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getPdo(): PDO
+    {
+        return $this->pdo;
+    }
+
+    /**
+     * @param PDO $pdo
+     * @return MyPDO
+     */
+    public function setPdo(PDO $pdo): MyPDO
+    {
+        $this->pdo = $pdo;
+        return $this;
+    }
+
+    /**
+     * @return PDOStatement
+     */
+    public function getPdosSelect(): PDOStatement
+    {
+        return $this->pdos_select;
+    }
+
+    /**
+     * @param PDOStatement $pdos_select
+     * @return MyPDO
+     */
+    public function setPdosSelect(PDOStatement $pdos_select): MyPDO
+    {
+        $this->pdos_select = $pdos_select;
+        return $this;
+    }
+
+    /**
+     * @return PDOStatement
+     */
+    public function getPdosUpdate(): PDOStatement
+    {
+        return $this->pdos_update;
+    }
+
+    /**
+     * @param PDOStatement $pdos_update
+     * @return MyPDO
+     */
+    public function setPdosUpdate(PDOStatement $pdos_update): MyPDO
+    {
+        $this->pdos_update = $pdos_update;
+        return $this;
+    }
+
+    /**
+     * @return PDOStatement
+     */
+    public function getPdosInsert(): PDOStatement
+    {
+        return $this->pdos_insert;
+    }
+
+    /**
+     * @param PDOStatement $pdos_insert
+     * @return MyPDO
+     */
+    public function setPdosInsert(PDOStatement $pdos_insert): MyPDO
+    {
+        $this->pdos_insert = $pdos_insert;
+        return $this;
+    }
+
+    /**
+     * @return PDOStatement
+     */
+    public function getPdosDelete(): PDOStatement
+    {
+        return $this->pdos_delete;
+    }
+
+    /**
+     * @param PDOStatement $pdos_delete
+     * @return MyPDO
+     */
+    public function setPdosDelete(PDOStatement $pdos_delete): MyPDO
+    {
+        $this->pdos_delete = $pdos_delete;
+        return $this;
+    }
+
+    /**
+     * @return PDOStatement
+     */
+    public function getPdosSelectAll(): PDOStatement
+    {
+        return $this->pdos_selectAll;
+    }
+
+    /**
+     * @param PDOStatement $pdos_selectAll
+     * @return MyPDO
+     */
+    public function setPdosSelectAll(PDOStatement $pdos_selectAll): MyPDO
+    {
+        $this->pdos_selectAll = $pdos_selectAll;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNomTable(): string
+    {
+        return $this->nomTable;
+    }
+
+    /**
+     * @param string $nomTable
+     * @return MyPDO
+     */
+    public function setNomTable(string $nomTable): MyPDO
+    {
+        $this->nomTable = $nomTable;
+        return $this;
+    }
+
+
+}
+
+?>
