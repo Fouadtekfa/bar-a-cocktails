c422d70e9cca3f03fecf571fe5db9e88d8fd11ea
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-02-19 21:07:28 +0100

    rajout du bouton ajout

--- public/Controlleur/CRUD/CRUD_Utensiles.php
+++ public/Controlleur/CRUD/CRUD_Utensiles.php
@@ -1,7 +1,55 @@
-<?php
+<?php 
 
-session_start();
+require_once "../../imports.php";
+require_once "../../Modele/EntiteUtensile.php";
+$myPDO = $_ENV['myPdo'];
+$myPDO->setNomTable('Utensile');
+$contenu = "";
+$etat="";
+if (isset($_GET['action'])){
+    switch ($_GET['action']) {
+        case 'insererUtensile': {
+            $nbUtensiles = $myPDO->getCountValue();
+            echo $nbUtensiles;
+            echo  $_GET['nom'];
+                        
+            $contenu = array(
+                "u_id" => "null",
+                "u_nom" => $_GET['nom']
+            );
 
-if(!isset($_GET['action']))
-    $_GET['action'] = 'init';
+            $_SESSION['etat'] = 'creation';
+            break;        
+        }
+        case 'modifierUtensile': {
+            $nbUtensiles = $myPDO->getCountValue();
+            $contenu = array(
+                "u_id" => $_GET['nom'],
+                "u_nom" => $_GET['nom']
+            );
+
+            $_SESSION['etat'] = 'modification';
+            break;        
+        }
+    }
+} 
+
+if (isset($_SESSION['etat'])) {
+    switch ($_SESSION['etat']) {
+        case 'creation':
+            $etat.="creation";
+            $myPDO->insert($contenu);
+            $_SESSION['etat'] = 'créé';
+            ?> <script>  document.location.href = '../../Vue/utensiles.php';  </script> <?php
+            break;
+
+    }
+}
+    
+
 ?>
+<p><?php foreach($contenu as $value) {
+        echo $value;
+    }
+    
+    echo $etat?></p>
