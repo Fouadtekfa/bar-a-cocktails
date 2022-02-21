3d97f03d8eda6e13d30da3731fdd413e6fac4e51
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-02-21 12:03:58 +0100

    ajout de sessions

--- public/imports.php
+++ public/imports.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+session_unset();
 require_once "Controlleur/MyPDO.php";
 require_once "Controlleur/connexion.php";
 $_ENV['myPdo'] = new MyPDO($_ENV['sgbd'], $_ENV['host'], $_ENV['db'], $_ENV['user'], $_ENV['pwd']);
