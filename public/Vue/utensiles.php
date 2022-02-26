d6bcffaa67a8bbae7aa222ed4138fa359c574653
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-02-26 02:34:30 +0100

    mis a jour du boutton retour

--- public/Vue/utensiles.php
+++ public/Vue/utensiles.php
@@ -17,7 +17,7 @@ class vueUtensiles {
                         </head>
                         <h1>Ustensiles</h1>
                         <div class="arrow" id="retour">
-                        <img src="../../images/retour.png" alt="retour" class="retour" onclick="history.back()">
+                        <a href="../../"><img src="../../images/retour.png" alt="retour" class="retour" ></a>
                         </i>
                         </div>                
                     <body>';
