4c3c44f8b53a0e57ccb06df541c854ffdfd00e38
Nadia Hariri <nadia.hariri@etu.univ-lehavre.fr>
2022-02-28 15:50:33 +0100

    Ajout des vue de ingrédient et verre

--- public/Vue/ingredients.php
+++ public/Vue/ingredients.php
@@ -1,61 +1,115 @@
-<?php 
-ini_set('display_errors','on');
-include "connexion.php";
-include "MyPDO.php";
-include "Session.php";
+<?php
+
+namespace bar;
+class ingredients
+{
+
+
+    public function getHTMLIngredient(EntiteG07_Ingredient $ingr): string
+    {
+
+        $ch ="<h1 class='text-center font-weight-bold text-white'>Information sur l'identifiant rechercher </h1>";
+        $ch .="<div class='container '>";
+        $ch .="<div class='row'>";
+        $ch .="<div class='col'>";
+        $ch .="</div>";
+        $ch .="<div class='col'>";
+        $ch .="<table class='table'>";
+        $ch .="<div class='columns'><div class='column is- '></div><div class='column '>";
+        $ch .="<thead class='thead-dark'>";
+        $ch .="<tr >";
+        $ch .="<th class='text-center'>v_id</th>";
+        $ch .="<th class='text-center'>v_type</th>";
+        $ch .="</tr></thead><tbody>";
+        $ch .= "<tr class='table-primary text-center'>";
+        $ch .= "<th class='text-center'>".$ingr->getIId()."</th>";
+        $ch .= "<th class='text-center'>".$ingr->getINom()."</th>";
+        $ch .= "<th class='text-center'>".$ingr->getIType()."</th>";
+        $ch .= "<th class='text-center'>".$ingr->getIQteStockee()."</th>";
+        $ch .= "<th class='text-center'>".$ingr->getIUniteStockee()."</th>";
+        $ch .= "</tr>";
+        $ch .= "</tbody></table>";
+        $ch .= "</div>";
+        $ch .= "<div class='col'>";
+        $ch .= "</div></div></div>";
+        return $ch;
+
+       
+
+    }
+
+    
+    public function getFormulaireIngredient(array $assoc): string
+    {
+      $ch ="<div class='container '>";
+      $ch .="<div class='row'>";
+      $ch .="<div class='col'>";
+      $ch .="</div>";
+      $ch .="<div class='col'>";
+      $ch .= "<div class='box text-success ' id='box4' id='titre2'>";
+      $ch .= " <form action='".$_SERVER['PHP_SELF']."' method='GET'>\n";
+        $i=0;
+        foreach ($assoc as $col => $val) {
+            if (is_array($val)) {
+              if($i==0){
+                $ch .= "<input class='input  mt-3 mb-3 ' name='$col' type='hidden' value='".$val['default']."'/>\n";
+              }else
+                $ch .= "$col : <input class=' input mt-3 mb-3' name='$col' type='".$val['type']
+                                  ."' value='".$val['default']."'/>\n";
+            }
+            else{
+
+                $ch .= "$col : <input class='input text-danger  mt-3 mb-3' type='$val' name='$col' />\n";
+              }
+                ++$i;
+        }
+
+
+          $ch .= "<center><input class='button is-danger bg-success is-outlined' type='submit' name='Valider' value='Sauvegarder'/></center>";
+          $ch .="</div'>";
+          
+
+
+          $ch .="</form>";
+          $ch .=" </div>";
+          $ch .="</div>";
+         $ch .="<div class='col'>";
+         $ch .="</div>";
+         $ch .="</div>";
+         $ch .="</div>";
+         return $ch;
+         }
+
+
+    public function getAllIngredient(array $tabEntiteG07_Ingredient): string
+    {
+        $ch = "";
+        foreach ($tabEntiteG07_Ingredient as $Ingredients) {
+            $ch.="";
+            if ($Ingredients instanceof EntiteG07_Ingredient) {
+                $id=$Ingredients->getIId();
+                $nom=$Ingredients->getINom();
+                $type= $Ingredients->getIType();
+                $IQteStockee= $Ingredients->getIQteStockee();
+                $UniteStockee=$Ingredients->getIUniteStockee();
+                $ch.="<div class='box' id='box4'>";
+                $ch.="<center>";
+                $ch .= "<img id='ronger' src='../../images/ingredients/$id.jpg'>";
+                $ch .= "<h1 class='text-primary mt-3 mb-3'>i_id</h1>".$id."<br>";
+                $ch .= "<h1 class='text-primary mt-3 mb-3'>i_nom</h1>".$nom."<br>";
+                $ch .= "<h1 class='text-primary mt-3 mb-3'>i_type</h1>".$type."<br>";
+                $ch .= "<h1 class='text-primary mt-3 mb-3'>i_qteStockee</h1>".$IQteStockee."<br>";
+                $ch .= "<h1 class='text-primary mt-3 mb-3'>i_uniteStockee</h1>".$UniteStockee."<br>";
+                $ch .= "<a href='../CRUD/Formulaire/formulaireIngredient.php?action=update&i_id=".$id."'><input class='button is-fullwidth has-background-info is-white is-outlined  mt-3 mb-3' type='submit' name='envoi' value='Modifier' /></a>";
+                $ch .= "<a href='../CRUD/Formulaire/formulaireIngredient.php?action=delete&i_id=".$id."'><input class='button is-fullwidth has-background-danger is-white is-outlined mt-3 mb-3' type='submit' name='envoi' value='Supprimer' /></a>";
+                $ch.="</div>";
+            }
+            $ch .= "</center>\n";
+        }
+        return $ch;
+    }
+
+
+
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/cocktails.css">
-    <title>Cocktail</title>
-</head>
-<body>
-    <h1>Ingredients</h1>
-    <div class="arrow" id="retour">
-    <img src="../images/retour.png" alt="retour" class="retour">
-    </i>
-    </div>
-    <div class="tableContainer">
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">col1</th>
-                    <th scope="col">col2</th>
-                    <th scope="col">col3</th>
-                    <th scope="col" colspan="3" style="text-align: center;">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
-                    for($i = 0 ; $i <= 5 ; $i++) { ?>
-
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td class="td_buttons_actions"><button type="button" class="btn btn-primary">Ajouter</button></td>
-                            <td class="td_buttons_actions"><button type="button" class="btn btn-warning">Editer</button></td>
-                            <td class="td_buttons_actions"><button type="button" class="btn btn-danger">Supprimer</button></td>
-                        </tr>
-                    
-                        <?php } ?>
-            </tbody>
-        </table>    
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="../js/ingredients.js"></script>
-
-</body>
-</html>
