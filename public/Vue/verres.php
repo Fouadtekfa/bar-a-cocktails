ffc939afd5d01e94edc307c6fce43fa27fcffa9d
nadia.hariri@etu.univ-lehavre.fr <nadiahariri94@gmail.com>
2022-03-02 16:08:17 +0100

    classe Verre Ingredients fonctionelle CRUD complet

--- public/Vue/verres.php
+++ public/Vue/verres.php
@@ -1,58 +1,106 @@
-<?php 
+<?php
 
-?>
+namespace bar;
+class vueVerres {
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/cocktails.css">
-    <title>Cocktail</title>
-</head>
-<body>
-    <h1>Verres</h1>
-    <div class="arrow" id="retour">
-    <img src="../images/retour.png" alt="retour" class="retour">
-    </i>
-    </div>
-    <div class="tableContainer">
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">col1</th>
-                    <th scope="col">col2</th>
-                    <th scope="col">col3</th>
-                    <th scope="col" colspan="3" style="text-align: center;">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
-                    for($i = 0 ; $i <= 5 ; $i++) { ?>
+    public function getDebutHTML($title, $lienRetour) : string {
+        $corps =    '<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                            <link rel="stylesheet" href="../../css/style.css">
+                            <link rel="stylesheet" href="../../css/utensiles.css">
+                            <title>'.$title.'</title>
+                        </head>
+                        <h1>VERRES</h1>
+                        <div class="arrow" id="retour">
+                        <a href="'.$lienRetour.'"><img src="../../images/retour.png" alt="retour" class="retour" ></a>
+                        </i>
+                        </div>                
+                    <body>';
+        return $corps;
+    }
 
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td class="td_buttons_actions"><button type="button" class="btn btn-primary">Ajouter</button></td>
-                            <td class="td_buttons_actions"><button type="button" class="btn btn-warning">Editer</button></td>
-                            <td class="td_buttons_actions"><button type="button" class="btn btn-danger">Supprimer</button></td>
-                        </tr>
-                    
-                        <?php } ?>
-            </tbody>
-        </table>    
-    </div>
+    public function getHTMLUpdate(array  $verre) : string {
+        $corps = "";
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="../js/verres.js"></script>
+        $corps .=    '<div class="insertContainer" id="insertUpdateContainer">'.
+            '<form id="updateUtensileForm"  method="get">'.
+            '<div class="form-group">';
+        foreach ($verre as $col => $val) {
+            if (is_array($val)) {
+                $hide = "";
+                if($col == 'v_id') $hide = 'hidden';
+                $corps.='       <label for="name" class="rowsInformation" '.$hide.'>'.$val['titre'].'</label>
+                                        <input type='.$val['type'].' class="form-control" '.$hide.'  id="v_id" name="'.$col.'" placeholder="Type de Verre" value="'.$val['default'].'" >';
 
-</body>
-</html>
+
+            }
+        }
+        $corps.='            </div>
+                                    <button type="submit" class="btn btn-primary">Modifier</button>
+                                </form>
+                            </div>';
+
+        return $corps;
+    }
+
+    public function getHTMLALL($va) : string {
+
+        $corps = '<div id="informationEntite">
+                    <div class="buttonContainer">
+                        <form id="insererUtensileFormButton"  method="post" action="?action=creat">
+                        <button type="submit" class="btn btn-primary" id="btn_ajouter">Ajouter</button>
+                        </form>
+                    </div>
+                    <div class="tableContainer">
+                        <table class="table">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">id</th>
+                                    <th scope="col">Type de Verre</th>
+                                    <th scope="col" colspan="3" style="text-align: center;">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>';
+
+        foreach ($va as $valeur){
+            if ($valeur instanceof EntiteVerre) {
+
+                $corps.= '          <tr>
+                                    <th scope="row">'. $valeur->getVId() .'</th>
+                                    <td class="rowsInformation">'. $valeur->getVType() . '</td>
+                                    <td class="td_buttons_actions"><a href="?action=update&v_id='.$valeur->getVId().'">
+                                    <button type="button" class="btn btn-warning etapes-btn">Editer</button></a></td>
+                                    <td class="td_buttons_actions">
+                                    <a href="?action=delete&v_id='.$valeur->getVId().'">
+                                    <button type="button" class="btn btn-danger">Supprimer</button></a></td>
+                                </tr>';
+            }
+        }
+
+        $corps.= '      </tbody>
+                       </table>    
+                    </div>
+                </div> ';
+        return $corps;
+    }
+
+    public function getHTMLInsert() : string {
+        $corps = '<div class="insertContainer" id="insertContainer">
+                    <form id="addUtensileForm"  method="get" action="CRUD_Verres.php">
+                        <div class="form-group">
+                            <label for="name" class="rowsInformation">Type de Verre</label>
+                            <input type="text" class="form-control" id="name" name="type" placeholder="Type de Verre">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Ajout</button>
+                    </form>
+                </div>';
+
+        return $corps;
+    }
+
+}
